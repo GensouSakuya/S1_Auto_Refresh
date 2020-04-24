6f53a1bc0492c8e37501430a05650f6d8452c5c4
GensouSakuya <zyqwert2@gmail.com>
2020-04-24 18:49:54 +0800

    增加调试项目

---
+++ src/MyPlugins/Plugins.S1.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Plugins.S1.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+            var key = args[0];
+            Console.WriteLine($"initKey:{key}");
+
+            var keeper = new S1Keeper(key);
+            keeper.Start();
+
+
+            Console.ReadKey();
+        }
+    }
+}
