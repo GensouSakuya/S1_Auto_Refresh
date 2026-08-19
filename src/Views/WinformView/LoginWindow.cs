20e98bf015060bed4a2544dce1b7434b5b5610d5
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2026-08-19 17:32:48 +0800

    更新user-agent

--- src/Views/WinformView/LoginWindow.cs
+++ src/Views/WinformView/LoginWindow.cs
@@ -48,7 +48,7 @@ namespace ForumTool.Winform
             CefSettings settings = new CefSettings()
             {
                 //todo send ua to keeper
-                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.44"
+                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36 Edg/151.0.0.0"
             };
             if(Cef.IsInitialized == false)
                 Cef.Initialize(settings);
